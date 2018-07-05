d90d6773068b701103b369208c6640fb097bea7b
Raphael Pierzina <raphael@hackebrot.de>
2018-07-05 18:45:28 +0200

    Add missing install requirement to setup.py

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setuptools.setup(
     package_dir={"": "src"},
     include_package_data=True,
     zip_safe=False,
-    install_requires=["click", "requests", "pytoml"],
+    install_requires=["click", "requests", "pytoml", "attrs"],
     entry_points={"console_scripts": ["labels = labels.cli:labels"]},
     python_requires=">=3.6",
     classifiers=[
