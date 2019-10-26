5e4af2d724edc181ab0a649bbc8ed61d549f4143
Mfon Eti-mfon <mfonetimfon@gmail.com>
2019-10-26 01:19:49 +0100

    Write util to infer repo name from cwd

--- src/labels/utils.py
+++ src/labels/utils.py
@@ -7,3 +7,11 @@ def get_owner_from_cwd() -> str:
         subprocess.check_output(["git", "remote", "get-url", "origin"]).decode().strip()
     )
     return origin_url.split("/")[-2]
+
+
+def get_repo_from_cwd() -> str:
+    """Return the name of the remote named origin in the current working directory."""
+    origin_url = (
+        subprocess.check_output(["git", "remote", "get-url", "origin"]).decode().strip()
+    )
+    return origin_url.split("/")[-1].split('.')[-2]
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -7,3 +7,11 @@ def test_get_owner_from_cwd(tmp_local_repo) -> None:
     """
     with tmp_local_repo.as_cwd():
         assert utils.get_owner_from_cwd() == "hackebrot"
+
+
+def test_get_repo_from_cwd(tmp_local_repo) -> None:
+    """Test that the repository name can be infered from the
+    local git repo int the current working directory.
+    """
+    with tmp_local_repo.as_cwd():
+        assert utils.get_repo_from_cwd() == "pytest-covfefe"
