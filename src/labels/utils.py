d7cc29cf0cb59202e49b866c1a3ba78e4067cb57
Mfon Eti-mfon <mfonetimfon@gmail.com>
2019-10-26 13:54:55 +0100

    Infer for remote having either HTTPS or SSH url
    Implement url format/protocol agnostic utility for extracting
    owner and repo name from the string representation of a remote url.

--- src/labels/utils.py
+++ src/labels/utils.py
@@ -1,3 +1,4 @@
+import re
 import subprocess
 import typing
 
@@ -7,5 +8,14 @@ def get_owner_and_repo_from_cwd() -> typing.Tuple[str, str]:
     origin_url = (
         subprocess.check_output(["git", "remote", "get-url", "origin"]).decode().strip()
     )
-    parts = origin_url.split("/")
-    return (parts[-2], parts[-1].split('.')[-2])
+    return _extract_o_and_r(origin_url)
+
+
+def _extract_o_and_r(url: str) -> typing.Tuple[str, str]:
+    """Return the owner and repo name of a remote given its SSH or HTTPS url.
+
+    HTTPS url format -> 'https://github.com/user/repo.git'
+    SSH   url format -> 'git@github.com:user/repo.git'
+    """
+    parts = re.split(r"[@/:.]+", url)
+    return (parts[-3], parts[-2])
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -7,3 +7,27 @@ def test_get_owner_and_repo_from_cwd(tmp_local_repo, owner: str, repo: str) -> N
     """
     with tmp_local_repo.as_cwd():
         assert utils.get_owner_and_repo_from_cwd() == (owner, repo)
+
+
+def test_extract_o_and_r_from_remote_https_url() -> None:
+    """Test extraction of owner and repo names from HTTPS remote url string."""
+    remote_url = "https://github.com/hackebrot/pytest-covfefe.git"
+    expected_owner = "hackebrot"
+    expected_repo = "pytest-covfefe"
+
+    gotten_owner, gotten_repo = utils._extract_o_and_r(remote_url)
+
+    assert gotten_owner == expected_owner
+    assert gotten_repo == expected_repo
+
+
+def test_extract_o_and_r_from_remote_ssh_url() -> None:
+    """Test extraction of owner and repo names from SSH remote url string."""
+    remote_url = "git@github.com:hackebrot/pytest-covfefe.git"
+    expected_owner = "hackebrot"
+    expected_repo = "pytest-covfefe"
+
+    gotten_owner, gotten_repo = utils._extract_o_and_r(remote_url)
+
+    assert gotten_owner == expected_owner
+    assert gotten_repo == expected_repo
